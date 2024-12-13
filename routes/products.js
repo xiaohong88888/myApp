a0ca297757e7e283433c352efe31cea8bcd9d531
xiaohong88888 <xiaohong.zhou@student.hogent.be>
2024-12-13 20:31:08 +0100

    Initial commit

---
+++ routes/products.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("test");
+});
+
+module.exports = router;
